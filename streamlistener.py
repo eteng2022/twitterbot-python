565ff50ee3768250b2042cd48c3a0977b57c136a
Emily Teng <44331102+eteng2022@users.noreply.github.com>
2022-01-08 05:55:31 -0800

    Update streamlistener.py

--- streamlistener.py
+++ streamlistener.py
@@ -1,39 +1,27 @@
 # import dependencies
 import tweepy
 
-# create new class "StreamListener" 
-# takes in tweepy.StreamListener as a parameter
+# create new class that takes in tweepy.Stream as a parameter
 class StreamListener(tweepy.Stream):
-  def __init__(self, apikey, apisecret, accesstoken, accesssecret, api, arr):
+  def __init__(self, apikey, apisecret, accesstoken, accesssecret, api):
     super().__init__(apikey, apisecret, accesstoken, accesssecret)
     self.api = api
     self.me = api.verify_credentials()
-    self.arr = arr
   
   # the function containing the logic on what to do for each tweet
   def on_status(self, tweet):
-    # We only want the bot to retweet original tweets, not replies.
-    # We also don't want the bot to retweet itself
-    if tweet.user.id == self.me.id or hasattr(tweet, 'retweeted_status'):
-      print("skip rts or own tweet")
+    # bot will not retweet itself or already retweeted tweets
+    # bot will only retweet original tweets
+    if tweet.retweeted or tweet.user.id == self.me.id or hasattr(tweet, 'retweeted_status'):
+      print("skip retweet or own tweet")
       return
     
-    # don't retweet if keywords are not directly found in text
-    found = False
-    for s in arr:
-        found = found or s in tweet.text
-    if not found:
-        print("keyword not found")
-        return
-    
-    # If we haven't retweeted this tweet yet, retweet it   
-    if not tweet.retweeted:
-      try: 
-        print(f"the current tweet is {tweet.text} by {tweet.user.screen_name}")
-        self.api.retweet(tweet.id)
-        print("Tweet retweeted successfully")
-      except Exception as e:
-        print(e)
-  # the function containing the logic in case there is an error  
-  def on_error(self, status):
-    print(f"Error while retweeting: {status}")
+    # functionality that retweets and likes the current tweet
+    try: 
+      print(f"the current tweet is {tweet.text} by {tweet.user.screen_name}")
+      self.api.create_favorite(tweet.id)
+      print("tweet liked successfully")
+      self.api.retweet(tweet.id)
+      print("tweet retweeted successfully")
+    except Exception as e:
+      print(e)
